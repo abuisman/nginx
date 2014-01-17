9e7b9ce20e71894e46e7bb180064522334a85a4d
Achilleas Buisman <accounts@abuisman.nl>
2014-01-17 21:04:42 +0100

    Rename to older

--- recipes/http_dav_module.rb
+++ recipes/http_dav_module.rb
@@ -20,4 +20,4 @@
 #
 
 node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | ['--with-ngx_http_dav_module']
+  node.run_state['nginx_configure_flags'] | ['--with-http_dav_module']
